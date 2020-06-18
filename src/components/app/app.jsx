71125ed648a349ad2cffeb9774f8c82fc7670b71
Olga <vesnushka.ol@mail.ru>
2020-06-18 18:34:10 +0200

    настраивает роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import QuestionArtist from "../question-artist/question-artist.jsx";
+import QuestionGenre from "../question-genre/question-genre.jsx";
 
 const welcomeButtonHandler = () => {};
 
 const App = (props) => {
   const {errorsCount} = props;
   return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtist />
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
