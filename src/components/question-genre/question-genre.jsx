bf682642cc0bee4cd25533f7b4a58ff946446535
Olga <vesnushka.ol@mail.ru>
2020-06-18 20:50:30 +0200

    дорабатывает компоненты

--- src/components/question-genre/question-genre.jsx
+++ src/components/question-genre/question-genre.jsx
@@ -5,11 +5,17 @@ import {GameType} from "../../const";
 class QuestionGenre extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      answers: [false, false, false, false],
+    };
   }
 
   render() {
-    const {question} = this.props;
+    const {onAnswer, question} = this.props;
+    const {answers: userAnswers} = this.state;
     const {answers, genre} = question;
+
     return (
       <section className="game game--genre">
         <header className="game__header">
@@ -20,7 +26,7 @@ class QuestionGenre extends PureComponent {
 
           <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
             <circle className="timer__line" cx="390" cy="390" r="370"
-              style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"/>
+              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
           </svg>
 
           <div className="game__mistakes">
@@ -32,9 +38,15 @@ class QuestionGenre extends PureComponent {
 
         <section className="game__screen">
           <h2 className="game__title">Выберите {genre} треки</h2>
-          <form className="game__tracks">
+          <form
+            className="game__tracks"
+            onSubmit={(evt) => {
+              evt.preventDefault();
+              onAnswer(question, this.state.answers);
+            }}
+          >
             {answers.map((answer, i) => (
-              <div key={answer.genre} className="track">
+              <div key={`${i}-${answer.src}`} className="track">
                 <button className="track__button track__button--play" type="button"></button>
                 <div className="track__status">
                   <audio
@@ -44,8 +56,17 @@ class QuestionGenre extends PureComponent {
                 <div className="game__answer">
                   <input className="game__input visually-hidden" type="checkbox" name="answer"
                     value={`answer-${i}`}
-                    id={`answer-${i}`} />
-                  <label className="game__check" htmlFor="answer-1">Отметить</label>
+                    id={`answer-${i}`}
+                    checked={userAnswers[i]}
+                    onChange={(evt) => {
+                      const value = evt.target.checked;
+
+                      this.setState({
+                        answers: [...userAnswers.slice(0, i), value, ...userAnswers.slice(i + 1)],
+                      });
+                    }}
+                  />
+                  <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
                 </div>
               </div>
             ))};
@@ -59,6 +80,7 @@ class QuestionGenre extends PureComponent {
 }
 
 QuestionGenre.propTypes = {
+  onAnswer: PropTypes.func.isRequired,
   question: PropTypes.shape({
     answers: PropTypes.arrayOf(PropTypes.shape({
       src: PropTypes.string.isRequired,
