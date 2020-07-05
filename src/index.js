8875d18765075be9e856e73835938f34e06ccbc7
Olga <vesnushka.ol@mail.ru>
2020-07-05 10:50:04 +0200

    настроит работу приложения с редьюсером

--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@ import ReactDOM from "react-dom";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import App from "./components/app/app.jsx";
-import {questions} from "./mock/questions";
 import {reducer} from "./reducer.js";
 
-const Settings = {
-  ERRORS_COUNT: 3
-};
-
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
-      <App
-        errorsCount={Settings.ERRORS_COUNT}
-        questions={questions}
-      />
+      <App />
     </Provider>,
     document.querySelector(`#root`)
 );
